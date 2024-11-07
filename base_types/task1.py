e8ed0bc324cf8361833ed73d535a1fe35970d80a
Alexander Gaiosh <alexander.gaiosh@gmail.com>
2024-11-07 22:18:17 +0300

    Добавил проверку на то, что введено 5 символов

--- base_types/task1.py
+++ base_types/task1.py
@@ -8,5 +8,11 @@
     30789 -> 38709
 """
 
-print('Введите пятизначное число: ')
-print(number := input(), '->', number[0] + number[-2:0:-1] + number[-1])
+print('Введите пятизначное число:')
+number = input()
+
+if len(number) != 5:
+    print('Число должно быть пятизначиным')
+
+else:
+    print(number, '->', number[0] + number[-2:0:-1] + number[-1])
